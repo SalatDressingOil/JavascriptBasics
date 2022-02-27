57c3bef541d8381e2900c6f6d986c043bd3149d3
Алексей <@DESKTOP-D98N22D>
2022-02-27 23:30:19 +0300

    добавление style.css

---
+++ app.js
@@ -0,0 +1,17 @@
+function fun(){
+
+}
+function ilonaKep() {
+    var img = new Image();
+    img.src = 'https://pic.rutubelist.ru/video/17/b1/17b100a0bcbc6e5e8d11101cde21aca7.jpg';
+    img.width = "570";
+    img.height = "320";
+    document.body.appendChild(img)
+  }
+//'https://pic.rutubelist.ru/video/17/b1/17b100a0bcbc6e5e8d11101cde21aca7.jpg'
+
+/*
+    <button onclick="fun()"> 
+        <img name = 'img' src = 'https://bugaga.ru/uploads/posts/2017-03/1489052030_kotik-hosiko-12.jpg' style="width:320px;height:320px;"/>
+    </button>
+*/
